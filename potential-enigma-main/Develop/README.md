184c291f4163c39952a6d79d5d3c51880c7d26e5
Cooper Griffin <coopergriffin@cmail.carleton.ca>
2024-01-22 18:22:53 -0500

    Updated index.js
    MVP working. Just have to fix some formatting

---
+++ potential-enigma-main/Develop/README.md
@@ -0,0 +1,32 @@
+# Hi
+
+## Description
+it mean hello
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+just say it 
+
+## Usage
+use your mouth
+
+## License
+![License](https://img.shields.io/badge/license-Apache-2.0-brightgreen)
+This application is covered under the Apache-2.0 license.
+
+## Contributing
+you know it
+
+## Tests
+can you hear em say it
+
+## Questions
+If you have any questions, you can reach me on GitHub: [lebarn james](https://github.com/lebarn james)
+or contact me via email: labarnjames at gmail
--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -6,42 +6,111 @@ const fs = require('fs');
 
 // Create an array of questions for user input
 const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'Enter your project title:',
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Enter a description:',
-  },
-  // Add more questions for other sections (installation, usage, etc.)
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title:',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:',
+      },
+      {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      },
 ];
 
 // Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFileSync(fileName, data);
-  console.log(`${fileName} generated successfully!`);
+    fs.writeFileSync(fileName, data);
+    console.log(`${fileName} generated successfully!`);
 }
+
+function generateLicenseBadge(license) {
+    if (license === 'None') {
+      return '';
+    }
+    return `![License](https://img.shields.io/badge/license-${encodeURIComponent(license)}-brightgreen)`;
+  }
+  
 
 // Create a function to initialize app
 function init() {
-  // Prompt the user for information
-  inquirer.prompt(questions).then((answers) => {
-    // Generate README content based on user input
-    const readmeContent = `# ${answers.title}
+    // Prompt the user for information
+    inquirer.prompt(questions).then((answers) => {
+        const licenseBadge = generateLicenseBadge(answers.license);
+        // Generate README content based on user input
+        const readmeContent = `# ${answers.title}
 
 ## Description
 ${answers.description}
 
-<!-- Add more sections and content based on user input -->
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+${licenseBadge}
+This application is covered under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+If you have any questions, you can reach me on GitHub: [${answers.githubUsername}](https://github.com/${answers.githubUsername})
+or contact me via email: ${answers.email}
 `;
 
-    // Write the README file
-    writeToFile('README.md', readmeContent);
-  });
+        // Write the README file
+        writeToFile('README.md', readmeContent);
+    });
 }
 
 // Function call to initialize app

c4a973a0fee569d6613b66ad1f3ec08006701627
Cooper Griffin <coopergriffin@cmail.carleton.ca>
2024-01-22 18:06:59 -0500

    Updated index.js
    Added functionality to functions and added base default question. Some commenting

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,13 +1,51 @@
-// TODO: Include packages needed for this application
+//Cooper Griffin 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// NPM Package Declaration 
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter your project title:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description:',
+  },
+  // Add more questions for other sections (installation, usage, etc.)
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFileSync(fileName, data);
+  console.log(`${fileName} generated successfully!`);
+}
+
+// Create a function to initialize app
+function init() {
+  // Prompt the user for information
+  inquirer.prompt(questions).then((answers) => {
+    // Generate README content based on user input
+    const readmeContent = `# ${answers.title}
+
+## Description
+${answers.description}
+
+<!-- Add more sections and content based on user input -->
+
+`;
+
+    // Write the README file
+    writeToFile('README.md', readmeContent);
+  });
+}
 
 // Function call to initialize app
 init();
+
+
+console.log("Hello");
